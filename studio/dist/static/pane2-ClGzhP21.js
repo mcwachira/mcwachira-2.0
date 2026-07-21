b8c2c31c5c24e2240ac5ba52685b7f9e20772cf6
mcwachira <mcwachira@outlook.com>
2026-07-21 20:38:00 +0300

    fixed issues that prevented fetching of blogs and projects and also issues caysing them not to render well

---
+++ studio/dist/static/pane2-ClGzhP21.js
@@ -0,0 +1,1 @@
+import{s as e}from"./sanity-WBjx8myL.js";export{e as default};
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6lt2m05z',
+    dataset: 'production',
+  },
+  studioHost: 'mcwachira-portfolio',
+  deployment: {
+    appId: 'hk1v7n6ldqqim5h1u2rn81t1',
+  },
+})
