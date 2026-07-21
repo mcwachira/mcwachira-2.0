fdffb09c69dd19181a9a9ae0e218639b72abf5a8
mcwachira <mcwachira@outlook.com>
2026-07-21 12:10:33 +0300

    fixed issues with remote images

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn.sanity.io"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
